30d0173c3da8634839189a814be7ccbcf429f4c2
aksamalaca <aksamala9999@gmail.com>
2023-03-17 09:48:04 +0700

    Moving to a new folder

---
+++ Tugas_01/mainBuku.java
@@ -0,0 +1,255 @@
+public class mainBuku{
+    public static void main(String[] args) {
+        Buku teknologi1 = new Buku();
+        teknologi1.setJudul("Perkembangan Teknologi di Era Globalisasi");
+        teknologi1.setKategori("Teknologi");
+        teknologi1.setPenulis("Anasita");
+        teknologi1.setTahun(2011);
+
+        Buku teknologi2 = new Buku();
+        teknologi2.setJudul("Buku Pengantar Teknologi Informasi");
+        teknologi2.setKategori("Teknologi");
+        teknologi2.setPenulis("Buhori Muslim");
+        teknologi2.setTahun(2017);
+
+        Buku teknologi3 = new Buku();
+        teknologi3.setJudul("Metode Penelitian Teknologi Informasi");
+        teknologi3.setKategori("Teknologi");
+        teknologi3.setPenulis("Ade Johar");
+        teknologi3.setTahun(2012);
+
+        Buku teknologi4 = new Buku();
+        teknologi4.setJudul("Komputer Cerdas Untuk Mahasiswa Teknologi Informasi");
+        teknologi4.setKategori("Teknologi");
+        teknologi4.setPenulis("Nur Nafi'iyah");
+        teknologi4.setTahun(2015);
+
+        Buku teknologi5 = new Buku();
+        teknologi5.setJudul("Buku Pengantar Teknologi Informatika Dan Komunikasi Data");
+        teknologi5.setKategori("Teknologi");
+        teknologi5.setPenulis("Bagaskoro");
+        teknologi5.setTahun(2017);
+
+        Buku filsafat1 = new Buku();
+        filsafat1.setJudul("Buku Ajar Filsafat Ilmu");
+        filsafat1.setKategori("Filsafat");
+        filsafat1.setPenulis("Agus Thoha");
+        filsafat1.setTahun(2011);
+
+        Buku filsafat2 = new Buku();
+        filsafat2.setJudul("Studi Dasar Filsafat");
+        filsafat2.setKategori("Filsafat");
+        filsafat2.setPenulis("Tazkiyah");
+        filsafat2.setTahun(2015);
+
+        Buku filsafat3 = new Buku();
+        filsafat3.setJudul("Filsafat Pendidikan Islam");
+        filsafat3.setKategori("Filsafat");
+        filsafat3.setPenulis("Afifuddin Harisah");
+        filsafat3.setTahun(2017);
+
+        Buku filsafat4 = new Buku();
+        filsafat4.setJudul("Filsafat Pendidikan Operasional");
+        filsafat4.setKategori("Filsafat");
+        filsafat4.setPenulis("Aswasulasikin");
+        filsafat4.setTahun(2014);
+
+        Buku filsafat5 = new Buku();
+        filsafat5.setJudul("Studi Dasar Filsafat");
+        filsafat5.setKategori("Filsafat");
+        filsafat5.setPenulis("Urip Sucipto");
+        filsafat5.setTahun(2017);
+
+        Buku sejarah1 = new Buku();
+        sejarah1.setJudul("Sejarah Dunia Lengkap");
+        sejarah1.setKategori("Sejarah");
+        sejarah1.setPenulis("Anthony Reid");
+        sejarah1.setTahun(2017);        
+
+        Buku sejarah2 = new Buku();
+        sejarah2.setJudul("Nusantara Sejarah Indonesia");
+        sejarah2.setKategori("Sejarah");
+        sejarah2.setPenulis("Sri Wintani");
+        sejarah2.setTahun(2012);  
+
+        Buku sejarah3 = new Buku();
+        sejarah3.setJudul("Jejak Mataram Islam di Yogyakarta");
+        sejarah3.setKategori("Sejarah");
+        sejarah3.setPenulis("Wiratna Sujarweni");
+        sejarah3.setTahun(2018);  
+        
+        Buku sejarah4 = new Buku();
+        sejarah4.setJudul("Dari Panggung Sejarah Bangsa");
+        sejarah4.setKategori("Sejarah");
+        sejarah4.setPenulis("Andi");
+        sejarah4.setTahun(2020);  
+
+        Buku sejarah5 = new Buku();
+        sejarah5.setJudul("Sejarah Runtuhnya Sriwijaya dan Majapahit");
+        sejarah5.setKategori("Sejarah");
+        sejarah5.setPenulis("Haidar Musyafa");
+        sejarah5.setTahun(2012);  
+
+        Buku agama1 = new Buku();
+        agama1.setJudul("Kamus Pintar Al-Qur'an");
+        agama1.setKategori("Agama");
+        agama1.setPenulis("Muhammad Chirzin");
+        agama1.setTahun(2019);  
+
+        Buku agama2 = new Buku();
+        agama2.setJudul("Pendidikan Agama Islam dan Budi Pekerti");
+        agama2.setKategori("Agama");
+        agama2.setPenulis("Ari Bayu");
+        agama2.setTahun(2004);  
+
+        Buku agama3 = new Buku();
+        agama3.setJudul("Pendidikan Agama Kristen dan Budi Pekerti");
+        agama3.setKategori("Agama");
+        agama3.setPenulis("Alexandra");
+        agama3.setTahun(2004);  
+
+        Buku agama4 = new Buku();
+        agama4.setJudul("Pribadi Akhlakul Kharimah");
+        agama4.setKategori("Agama");
+        agama4.setPenulis("A. Fatih Syuhud");
+        agama4.setTahun(2016);  
+
+        Buku agama5 = new Buku();
+        agama5.setJudul("Dari Siwaisme Jawa ke Agama Hindu Bali");
+        agama5.setKategori("Agama");
+        agama5.setPenulis("Andrea Acri");
+        agama5.setTahun(2018);  
+
+        Buku psikologi1 = new Buku();
+        psikologi1.setJudul("Dasar-Dasar Psikologi");
+        psikologi1.setKategori("Psikologi");
+        psikologi1.setPenulis("Tukiyo");
+        psikologi1.setTahun(2020);  
+
+        Buku psikologi2 = new Buku();
+        psikologi2.setJudul("Psikologi Umum");
+        psikologi2.setKategori("Psikologi");
+        psikologi2.setPenulis("Kenneth Carter");
+        psikologi2.setTahun(2010);  
+
+        Buku psikologi3 = new Buku();
+        psikologi3.setJudul("Psikologi Remaja");
+        psikologi3.setKategori("Psikologi");
+        psikologi3.setPenulis("Muhammad Ali");
+        psikologi3.setTahun(2018);  
+
+        Buku psikologi4 = new Buku();
+        psikologi4.setJudul("Psikologi Pendidikan");
+        psikologi4.setKategori("Psikologi");
+        psikologi4.setPenulis("Fattah");
+        psikologi4.setTahun(2016);  
+
+        Buku psikologi5 = new Buku();
+        psikologi5.setJudul("Psikologi Eksperimen");
+        psikologi5.setKategori("Psikologi");
+        psikologi5.setPenulis("Helen Pricilla");
+        psikologi5.setTahun(2012);  
+
+        Buku politik1 = new Buku();
+        politik1.setJudul("Politik dan Kebebasan");
+        politik1.setKategori("Politik");
+        politik1.setPenulis("Tom G. Palmer");
+        politik1.setTahun(2017);  
+
+        Buku politik2 = new Buku();
+        politik2.setJudul("Politik Pemerintah Desa di Indonesia");
+        politik2.setKategori("Politik");
+        politik2.setPenulis("Tirta Kusuma");
+        politik2.setTahun(2005);  
+
+        Buku politik3 = new Buku();
+        politik3.setJudul("Politik Kuasa Media");
+        politik3.setKategori("Politik");
+        politik3.setPenulis("Noam Chomsky");
+        politik3.setTahun(2013);  
+
+        Buku politik4 = new Buku();
+        politik4.setJudul("Politik Hukum di Indonesia");
+        politik4.setKategori("Politik");
+        politik4.setPenulis("Moh. Mahmud");
+        politik4.setTahun(2019);  
+
+        Buku politik5 = new Buku();
+        politik5.setJudul("Sejarah Politik Hukum di Indonesia");
+        politik5.setKategori("Politik");
+        politik5.setPenulis("Chaidir");
+        politik5.setTahun(2002);  
+
+        Buku fiksi1 = new Buku();
+        fiksi1.setJudul("Perempuan dan Literasi");
+        fiksi1.setKategori("Fiksi");
+        fiksi1.setPenulis("Anna Farida");
+        fiksi1.setTahun(2014);  
+
+        Buku fiksi2 = new Buku();
+        fiksi2.setJudul("Orang-orang Biasa");
+        fiksi2.setKategori("Fiksi");
+        fiksi2.setPenulis("Bayu");
+        fiksi2.setTahun(2010);  
+
+        Buku fiksi3 = new Buku();
+        fiksi3.setJudul("Perahu Kerta");
+        fiksi3.setKategori("Fiksi");
+        fiksi3.setPenulis("Dee Lestari");
+        fiksi3.setTahun(2017);  
+        
+        Buku fiksi4 = new Buku();
+        fiksi4.setJudul("Sebuah Seni Untuk Bersikap Bodo Amat");
+        fiksi4.setKategori("Fiksi");
+        fiksi4.setPenulis("Mark Manson");
+        fiksi4.setTahun(2013);  
+
+        Buku fiksi5 = new Buku();
+        fiksi5.setJudul("Berdamai dengan Badai");
+        fiksi5.setKategori("Fiksi");
+        fiksi5.setPenulis("Ananda Aldo");
+        fiksi5.setTahun(2019);  
+
+        teknologi1.display();
+        teknologi2.display();
+        teknologi3.display();
+        teknologi4.display();
+        teknologi5.display();
+
+        filsafat1.display();
+        filsafat2.display();
+        filsafat3.display();
+        filsafat4.display();
+        filsafat5.display();
+
+        sejarah1.display();
+        sejarah2.display();
+        sejarah3.display();
+        sejarah4.display();
+        sejarah5.display();
+
+        agama1.display();
+        agama2.display();
+        agama3.display();
+        agama4.display();
+        agama5.display();
+
+        psikologi1.display();
+        psikologi2.display();
+        psikologi3.display();
+        psikologi4.display();
+        psikologi5.display();
+
+        politik1.display();
+        politik2.display();
+        politik3.display();
+        politik4.display();
+        politik5.display();
+
+        fiksi1.display();
+        fiksi2.display();
+        fiksi3.display();
+        fiksi4.display();
+        fiksi5.display();
+    }
+}
