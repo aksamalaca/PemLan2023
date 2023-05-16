898eefac93959a903dd00deb7ec7e17ef84c819b
aksamalaca <aksamala9999@gmail.com>
2023-05-17 01:02:32 +0700

    BAB 7 Polymorphism

---
+++ BAB 7 Polymorphism/Kue.java
@@ -0,0 +1,22 @@
+import java.text.DecimalFormat;
+public abstract class Kue {
+    private String nama;
+    private double harga;
+
+    public Kue(String nama, double harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+    public String getNama() {
+        return nama;
+    }
+    public double getHarga() {
+        return harga;
+    }
+    public abstract double hitungHarga();
+
+    public String toString() {
+        DecimalFormat decimalFormat = new DecimalFormat("#,###");
+        return String.format("Nama kue: %s\nHarga kue: Rp %s", this.nama, decimalFormat.format(this.hitungHarga()));
+    }
+}
---
+++ BAB 7 Polymorphism/KuePesanan.java
@@ -0,0 +1,14 @@
+public class KuePesanan extends Kue{
+    private double berat;
+
+    public KuePesanan(String nama, double harga, double berat){
+        super(nama, harga);
+        this.berat = berat;
+    }
+    public double getBerat(){
+        return berat;
+    }
+    public double hitungHarga(){
+        return this.berat * super.getHarga();
+    }
+}
