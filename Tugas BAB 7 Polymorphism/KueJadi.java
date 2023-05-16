b34b5424c1c17784c9c943fed2d312ae8e62ea01
aksamalaca <aksamala9999@gmail.com>
2023-05-17 00:24:31 +0700

    BAB 7: Polymorphism

---
+++ Tugas BAB 7 Polymorphism/KueJadi.java
@@ -0,0 +1,14 @@
+public class KueJadi extends Kue{
+    private double jumlah;
+
+    public KueJadi(String nama, double harga, double jumlah){
+        super(nama, harga);
+        this.jumlah = jumlah;
+    }
+    public double getJumlah(){
+        return jumlah;
+    }
+    public double hitungHarga(){
+        return this.jumlah * super.getHarga() * 2;
+    }
+}
